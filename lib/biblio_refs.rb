612f28f523e3faea6373eb3e068c383453e29c34
Sergio Rodríguez <alu0100699968@ull.edu.es>
2015-11-18 19:10:27 +0000

    Añadido código para cumplir la expectativa de pertenencia a la jerarquía

--- lib/biblio_refs.rb
+++ lib/biblio_refs.rb
@@ -1,3 +1,4 @@
 require "biblio_refs/version"
 require "biblio_refs/referencia"
 require "biblio_refs/list"
+require "biblio_refs/publicaciones_periodicas"
---
+++ lib/biblio_refs/publicaciones_periodicas.rb
@@ -0,0 +1,8 @@
+module BiblioRefs
+  class PublicacionesPeriodicas < Referencia
+
+    def initialize
+    end
+    
+  end
+end
