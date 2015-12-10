a9436ebf97d940eef288f1e13d66ead949665875
Sergio Rodríguez <alu0100699968@ull.edu.es>
2015-12-10 11:43:03 +0000

    Implementada clase ArticuloRevista con el formato to_s adecuado

--- lib/biblio_refs/articulo_periodico.rb
+++ lib/biblio_refs/articulo_periodico.rb
@@ -1,7 +1,14 @@
 module BiblioRefs
   class ArticuloPeriodico < PublicacionesPeriodicas
 
-    def initialize
+    attr_accessor :nombre_periodico, :num_paginas
+
+    def initialize(autores, fecha_publicacion, titulo, nombre_periodico, num_paginas)
+      @autores = autores
+      @fecha_publicacion = fecha_publicacion
+      @titulo = titulo
+      @nombre_periodico = nombre_periodico
+      @num_paginas = num_paginas
     end
 
   end
--- lib/biblio_refs/articulo_revista.rb
+++ lib/biblio_refs/articulo_revista.rb
@@ -1,11 +1,12 @@
 module BiblioRefs
   class ArticuloRevista < PublicacionesPeriodicas
 
-    attr_accessor :paginas, :volumen, :eds
+    attr_accessor :titulo_obra, :paginas, :volumen, :eds
 
-    def initialize(autores, titulo, editorial, num_edicion, fecha_publicacion, volumen, paginas, eds)
+    def initialize(autores, titulo, titulo_obra, editorial, num_edicion, fecha_publicacion, volumen, paginas, eds)
       @autores = autores
       @titulo = titulo
+      @titulo_obra = titulo_obra
       @editorial = editorial
       @num_edicion = num_edicion
       @fecha_publicacion = fecha_publicacion
@@ -27,6 +28,32 @@ module BiblioRefs
         final += "  "
       end
       final.chop
+    end
+
+    def fecha_publicacion_to_s
+      "(" + fecha_publicacion.year.to_s + "). "
+    end
+
+    def eds_to_s
+      final = ""
+      if eds.kind_of?(Array)
+        eds.each do |ed|
+          final += ed
+          final += " & "
+        end
+        final[-1] = ""
+      else
+        final += eds
+        final += "  "
+      end
+      final.chop
+    end
+
+    def to_s
+      final = autores_to_s + fecha_publicacion_to_s
+      final += titulo_to_s + ". En " + eds_to_s + "(Eds.), "
+      final += titulo_obra.to_s + " (" + paginas.to_s + ") " + "(" + num_edicion_to_s + ") "
+      final += "(" + volumen.to_s + "). Lugar de publicación: " + editorial_to_s
     end
 
   end
