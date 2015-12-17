55d7b1b1dbace2c8af40b83cf14fa86150382bb5
Sergio Rodríguez <alu0100699968@ull.edu.es>
2015-12-17 14:13:45 +0000

    Fix de Coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-$LOAD_PATH.unshift File.expand_path('../lib', __FILE__)
-require 'biblio_refs'
 require 'coveralls'
 Coveralls.wear!
+$LOAD_PATH.unshift File.expand_path('../lib', __FILE__)
+require 'biblio_refs'
